6d73507c77cb171ba40c18acbc4f20c9359e9b2c
Yehuda Tarona <yehuda.tarona@gmail.com>
2021-03-02 11:03:41 +0200

    first commit

---
+++ js/animals.js
@@ -0,0 +1,164 @@
+const animals_ar = [
+    {
+        "id": 50267,
+        "pageURL": "https://pixabay.com/photos/owl-bird-animal-bird-of-prey-50267/",
+        "tags": "owl, bird, animal",
+        "previewURL": "https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267_150.jpg",
+        "webformatURL": "https://pixabay.com/get/g6a5249c8ec4f5887f9c81bd472931c58413daeecea72429b1743fb1bbefba987502e7e43069a712d0203338c5b012d72_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/ga6479d1424008aaa5f53a75522d954a957439e17f9aeb5ee13dd235cf5eb8008d50110c9627ff495c046ee4510f978c9_1280.jpg"
+
+    },
+    {
+        "id": 3601194,
+        "pageURL": "https://pixabay.com/photos/ara-parrot-yellow-macaw-bird-3601194/",
+        "tags": "ara, parrot, yellow macaw",
+        "previewURL": "https://cdn.pixabay.com/photo/2018/08/12/16/59/ara-3601194_150.jpg",
+        "webformatURL": "https://pixabay.com/get/gc3928ff92c0cfdfa07f78fe7b66fb1bb7dcbf893b4992a89eab1456aa8d13a4c51f1b76e3bf1fa78a1e8bab334a89cb44de1f6f51060594ef55b82fa93c77d5b_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/g50570dee8582fc759321788683e5a0276f6d3da6249d661ed1e4e82df79fbc11b6a40e346a9e596920bedba0af8e238346281e94798b58092c431b40b8f21bda_1280.jpg"
+    },
+    {
+        "id": 1979445,
+        "pageURL": "https://pixabay.com/photos/iceland-arctic-fox-fox-animal-1979445/",
+        "tags": "iceland, arctic fox, fox",
+        "previewURL": "https://cdn.pixabay.com/photo/2017/01/14/12/59/iceland-1979445_150.jpg",
+        "webformatURL": "https://pixabay.com/get/g96ae5a552cdc8c917113bbe3d03e350a8a907353713e395472162ff1e29c5bb9a0d743e82f345bafa792719641b62c23eeccb697ca4e5ffa3e5fe4ce780f649a_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/g0a95d0c686ceef16ed2fe5c4854865811dbe2ce4a7310be2c591649441e9da7c88f01b4eaef97158eedd840dd352436bb0444d081f3ee5b8acf33e45f3ccea51_1280.jpg"
+    },
+    {
+        "id": 2361247,
+        "pageURL": "https://pixabay.com/photos/sea-turtle-diving-animal-ocean-2361247/",
+        "tags": "sea, turtle, diving",
+        "previewURL": "https://cdn.pixabay.com/photo/2017/05/31/18/38/sea-2361247_150.jpg",
+        "webformatURL": "https://pixabay.com/get/gc127004cabca8949e5d15d2f8e3412390a87c372cfb7579fc84710284ec16230d934e88449b48c79723e0a659bd5014a282c2705afe6685917132786cafbf1cb_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/gfee33dd56825d22c8493e52c0adb3f814ead0fbc73a2fe909afcf04324cb0faba6a3697211ea102fa28534366ccec82d6590d0e4094f7e2fb42a16f885615750_1280.jpg"
+    },
+    {
+        "id": 2083492,
+        "pageURL": "https://pixabay.com/photos/cat-young-animal-curious-wildcat-2083492/",
+        "tags": "cat, young animal, curious",
+        "previewURL": "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_150.jpg",
+        "webformatURL": "https://pixabay.com/get/gb5eb970354a97b5a7ceca7c31877a09b53341869660f1b9e2c560140c7d545bdac330233f125c1b8894d28b3bc1709c2dcd03af34457536c0610d3455c646b3b_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/g5179ab415eab51df7d808069da3e4ed32283ee3d2d0da75567ea8fb99e57baaf68f9018561e32efc5cf02e55552fe80b02c8a01366ced995f21a9ffdc50544d4_1280.jpg"
+    },
+    {
+        "id": 203875,
+        "pageURL": "https://pixabay.com/photos/dolphin-animal-sea-ocean-203875/",
+        "tags": "dolphin, animal, sea",
+        "previewURL": "https://cdn.pixabay.com/photo/2013/11/01/11/13/dolphin-203875_150.jpg",
+        "webformatURL": "https://pixabay.com/get/gd9477deeabd681e13a60610989f32d39d44e0a25e36c8d04474c84301ec0b11e89dd08916bd909f79e5d8420924bbc79_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/gf4f2fe5455d5f7b8afd6e5a70350ac9579598c2b153ab28a283840fdb83f4f7e99d0276fd6f3d9c97261f1b714e6539983c3e0530f0038a939f9a8cac9fe34fb_1280.jpg"
+    },
+    {
+        "id": 2516641,
+        "pageURL": "https://pixabay.com/photos/dove-bird-animal-feather-plumage-2516641/",
+        "tags": "dove, bird, animal",
+        "previewURL": "https://cdn.pixabay.com/photo/2017/07/18/18/24/dove-2516641_150.jpg",
+        "webformatURL": "https://pixabay.com/get/gefcd7bf67a124bf908b5550ecfc0731549b86826e101f32b82d9b23c05086597a5c484392ecd79326f7f928a48713f6444b23b4c122cd89870b109ae53e4e08d_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/gca102fe9f63bf33930659559b9a247b1914927d2bf3125042e1941477f66ef5567edb38b3ceae1f85b3de49e5a104e97ef1db443e658392cd37de7e97040c56a_1280.jpg"
+
+    },
+    {
+        "id": 2107052,
+        "pageURL": "https://pixabay.com/photos/swan-water-white-water-bird-lake-2107052/",
+        "tags": "swan, water, white",
+        "previewURL": "https://cdn.pixabay.com/photo/2017/02/28/23/00/swan-2107052_150.jpg",
+        "webformatURL": "https://pixabay.com/get/g2b5e34befe2e18af6e4555b89ebfcd3f405c9540508abb87efcad522335485ba9a581cd6a1bac717990524d7641cd0082dd7721b99bf7f0e5af194090a3186b3_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/g799dd95fde268f0e3816a557382f19e99f41de46d43ba18649f60c67e5cbdfa34d1b64edf06f740326c3165329563cee4b77f49b4edcd235e98e3f3fa5241e1e_1280.jpg"
+    },
+    {
+        "id": 3576045,
+        "pageURL": "https://pixabay.com/photos/lion-predator-dangerous-mane-3576045/",
+        "tags": "lion, predator, dangerous",
+        "previewURL": "https://cdn.pixabay.com/photo/2018/07/31/22/08/lion-3576045_150.jpg",
+        "webformatURL": "https://pixabay.com/get/gedf2500d34144794ca82b5c269cce369590c9f4f6eb5ab427be5228934c4ab2009cbb73b5117f9f0a126e1b0ca0af077229404ef432ad002f815b9be28c90d67_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/g07199b1fb5786432244a311e21a857becf1b695b50cca737b6d0dd0a71d4792ced2d0bc467208c4efe4fccc7e36ac14f4268d1f2f09e08e51661064e1b37f11a_1280.jpg"
+    },
+    {
+        "id": 1883658,
+        "pageURL": "https://pixabay.com/photos/fox-animal-wildlife-red-macro-1883658/",
+        "tags": "fox, animal, wildlife",
+        "previewURL": "https://cdn.pixabay.com/photo/2016/12/05/11/39/fox-1883658_150.jpg",
+        "webformatURL": "https://pixabay.com/get/g7f4c1b28db4f7bd8d6c744ea17d355c3f7333d4f7d4e7c8e7e1cedcdbb53c58d60c0cf83d241828f87af79d4f8eae386e7e3b58eacbbb31b28d68cb123ffe248_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/g0ba9d013c41c108690df9f71214c22cc571564a2eaacbd075e28124e8318f5edb96592a51605d83cac9486ad1b2bae2e77ef60f2dd4bf05aec033f90ad49aa62_1280.jpg"
+    },
+    {
+        "id": 3099035,
+        "pageURL": "https://pixabay.com/photos/animal-horses-fauna-nature-cavalry-3099035/",
+        "tags": "animal, horses, fauna",
+        "previewURL": "https://cdn.pixabay.com/photo/2018/01/22/14/13/animal-3099035_150.jpg",
+        "webformatURL": "https://pixabay.com/get/g101290976da97dce386969377d7b08ac77f3f698d24f94b765ea14b4a41133df5a0376206396b616b9befd43746a567a22326ee34ea5aacf867fc0eaae7a393c_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/ga97c53f353681113e15d9a0c530904dd53a7fc970a3895c6e2dc277b2011dc38a1853e4d76aabb184a82f6aaa827fea55f0f0acbafaa918cc29b859adf5659ed_1280.jpg"
+    },
+    {
+        "id": 1045954,
+        "pageURL": "https://pixabay.com/photos/bird-fluttering-berries-wings-tit-1045954/",
+        "tags": "bird, fluttering, berries",
+        "previewURL": "https://cdn.pixabay.com/photo/2015/11/16/16/28/bird-1045954_150.jpg",
+        "webformatURL": "https://pixabay.com/get/g61202640f01463d4ae299bd65b291660435d4fa8419f807df62ee4bfeab7beb7b0b34112daf1d59a7e27b908bbc4426edb826d2821cc45697dba4a4fe2ef460c_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/gf2e61297547b5b2840ac895c5b3bcd4155a1d0d5f5122a19d4d73bbd3ff1e86cc3694f0cdc1337491d6345182fe9a5648de208d9d6a724b9a68a2e1c0960c171_1280.jpg"
+    },
+    {
+        "id": 19830,
+        "pageURL": "https://pixabay.com/photos/garden-flowers-butterfly-19830/",
+        "tags": "garden, flowers, butterfly",
+        "previewURL": "https://cdn.pixabay.com/photo/2012/03/01/00/55/garden-19830_150.jpg",
+        "webformatURL": "https://pixabay.com/get/ga8eb40acba8debbe7a648b2361e47a09f27f2b3a58774fde5679dff29666d7297043f550daea23f41f3268505dd0e8b5_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/g4fad661e79d634f9e0ef848e0468a0dbe8ff9e0ef38b6b8e1fb188b758858d53a5b231be59a8563f26ca6d2f0abc78c3_1280.jpg"
+    },
+    {
+        "id": 2535888,
+        "pageURL": "https://pixabay.com/photos/tiger-big-cat-big-cats-animal-world-2535888/",
+        "tags": "tiger, big cat, big cats",
+        "previewURL": "https://cdn.pixabay.com/photo/2017/07/24/19/57/tiger-2535888_150.jpg",
+        "webformatURL": "https://pixabay.com/get/g038cc9f138ce944847f17ec714b5d66b34de43d9c809af9bb19968e01f49f989858b7454e5b189bb190f5536212f6558f5c9340fbbc3064e8c2174ff6cbd8bf0_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/ge50b0cd082df7750419d952c53c78b3eac4d8fd87a5a4d79916d77eb97b0d7ed9c82f7c0ab8a46d9b569d28677ce31bf7dde7434862558b6ca52ae0b56417160_1280.jpg"
+    },
+    {
+        "id": 715588,
+        "pageURL": "https://pixabay.com/photos/fox-nature-animals-roux-fauna-715588/",
+        "tags": "fox, nature, animals",
+        "previewURL": "https://cdn.pixabay.com/photo/2015/04/10/01/41/fox-715588_150.jpg",
+        "webformatURL": "https://pixabay.com/get/gd35b130732a50e89faf9985f13d9753a2fa6df8fd3728c383340dab8d7d213673299955c1bca227000845a308895ef3a_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/g5d67d2e9b761321c955ab263961a70359c1bce1d37018dda16efbb2bdd74801f4e6d266f342ca7dee485c61c76fdc7fba34929b948fa119fc39dd7ecdc700cfa_1280.jpg"
+    },
+    {
+        "id": 2864647,
+        "pageURL": "https://pixabay.com/photos/wolf-forest-dark-predator-animal-2864647/",
+        "tags": "wolf, forest, dark",
+        "previewURL": "https://cdn.pixabay.com/photo/2017/10/18/16/08/wolf-2864647_150.jpg",
+        "webformatURL": "https://pixabay.com/get/g21b9454ae402af852105e979e94f95cb15a5407bc264e58fed9b3216ce9fd327db736e406392c7306c666645d490c6ea7f4c806e65be2fda071f72871b96ce9d_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/gcd0546ec6931dbe0ff44257263ceda8cf438d2c3ce623db591733b9a935609488cf408d6efd51afcff551fd018ca2fb29312e53c64721046d1d0d8b9a7dcefb5_1280.jpg"
+    },
+    {
+        "id": 984573,
+        "pageURL": "https://pixabay.com/photos/fallow-deer-stag-deer-wild-animal-984573/",
+        "tags": "fallow deer, stag, deer",
+        "previewURL": "https://cdn.pixabay.com/photo/2015/10/12/15/46/fallow-deer-984573_150.jpg",
+        "webformatURL": "https://pixabay.com/get/gba6fb45160fd72231eb06faa396b5a85721a2415eba2c8860fde1b3857d3cbfe3ee269421f1e3023ff50af2b828a60fe_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/g6b14cac75bba1b18ccde3552ef153439df68b7b8b0b2ca6af84308376f9d1657ab4f944f3a1843451749170ea0e470bde27d83180235426169687ece2c59dc7c_1280.jpg"
+    },
+    {
+        "id": 1045782,
+        "pageURL": "https://pixabay.com/photos/cat-animal-cat-portrait-cat-s-eyes-1045782/",
+        "tags": "cat, animal, cat portrait",
+        "previewURL": "https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782_150.jpg",
+        "webformatURL": "https://pixabay.com/get/g9b89a33c2d2fb0ef7bdd56abe60d1d68c6ce9866f0dcb30339821361ce5b61e41fafbb583226677dcaef5738791224c7fe9b33e567e88645f772176e23574c53_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/gfc04181f65bcd086a6061f3dcc1864c95d0fd8946a78d8175a6c4c0f8139f26cf3b8233bc961beff4111d6a7a4031d19227d6b71060cc75723ecfa22da6ec799_1280.jpg"
+    },
+    {
+        "id": 3012515,
+        "pageURL": "https://pixabay.com/photos/lion-roar-africa-animal-wildcat-3012515/",
+        "tags": "lion, roar, africa",
+        "previewURL": "https://cdn.pixabay.com/photo/2017/12/11/15/34/lion-3012515_150.jpg",
+        "webformatURL": "https://pixabay.com/get/g393115caed94afa6714f3b00b09c0df6d56a5c47bb6cd62235d71d0e5a8b2d01c757f1d3e8485b3447355abe75665750b88cafe7351b4468d56a8f7e5cc3df94_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/gdbaa18ae3ef086137fba642d382dbe45d8f00aee4c76291f7a6527e21567da2de168464a5ceff0646a97a85eb9b337c5fe969fbb7da5d311610081b6713127b2_1280.jpg"
+    },
+    {
+        "id": 468228,
+        "pageURL": "https://pixabay.com/photos/hedgehog-animal-baby-cute-small-468228/",
+        "tags": "hedgehog, animal, baby",
+        "previewURL": "https://cdn.pixabay.com/photo/2014/10/01/10/44/hedgehog-468228_150.jpg",
+        "webformatURL": "https://pixabay.com/get/g783fbd997f14a0639d311b2566497aa594e40e96aea673c8ae32ab2202a16ddb3ebbb79eadf361d9f95986f43c0645e6_640.jpg",
+        "largeImageURL": "https://pixabay.com/get/gbae5e75088e3266041460177fb64a9654b78449214801099ba97b8dad6e229fc325adead327a78331936ce4bc25e49d9d71e48f7c5bcd7e1c0156a8627ee70f3_1280.jpg"
+    }
+]
---
+++ js/memoryApp.js
@@ -0,0 +1,224 @@
+const $audio = document.getElementById("audio"),
+  $score = document.getElementById("score"),
+  $steps = document.getElementById("steps"),
+  $timer = document.getElementById("timer"),
+  $board = document.getElementById("board"),
+  $start = document.getElementById("start"),
+  cards = [
+    {
+      answer: "turtle",
+      image:
+        "https://cdn.pixabay.com/photo/2017/05/31/18/38/sea-2361247_960_720.jpg",
+    },
+    {
+      answer: "cat",
+      image:
+        "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg",
+    },
+    {
+      answer: "lion",
+      image:
+        "https://cdn.pixabay.com/photo/2018/07/31/22/08/lion-3576045_960_720.jpg",
+    },
+    {
+      answer: "fox",
+      image:
+        "https://cdn.pixabay.com/photo/2016/12/05/11/39/fox-1883658_960_720.jpg",
+    },
+    {
+      answer: "butterfly",
+      image:
+        "https://cdn.pixabay.com/photo/2012/03/01/00/55/garden-19830_960_720.jpg",
+    },
+    {
+      answer: "hedgehog",
+      image:
+        "https://cdn.pixabay.com/photo/2014/10/01/10/44/hedgehog-468228_960_720.jpg",
+    },
+    {
+      answer: "dolphin",
+      image:
+        "https://cdn.pixabay.com/photo/2013/11/01/11/13/dolphin-203875_960_720.jpg",
+    },
+    {
+      answer: "tiger",
+      image:
+        "https://cdn.pixabay.com/photo/2017/07/24/19/57/tiger-2535888_960_720.jpg",
+    },
+  ];
+
+let timerInterval,
+  selected = [],
+  timer = 120,
+  steps = 0,
+  score = 0;
+
+const shuffle = (arrayofItems) => {
+  let counter = arrayofItems.length;
+  while (counter > 0) {
+    let index = Math.floor(Math.random() * counter);
+    counter--;
+    let temp_ar = arrayofItems[counter];
+    arrayofItems[counter] = arrayofItems[index];
+    arrayofItems[index] = temp_ar;
+  }
+
+  return arrayofItems;
+};
+
+const countTime = () => {
+  timerInterval = setInterval(() => {
+    --timer;
+    $timer.innerHTML = timer;
+    if (!timer) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+};
+
+const countStep = () => {
+  ++steps;
+  $steps.innerHTML = steps;
+};
+
+const checkIfGameOver = () =>{
+    clearInterval(timerInterval);
+    const openCards = (document.getElementsByClassName('open')).length;
+    if(cards.length * 2 == openCards){
+        setTimeout(() => {
+            Swal.fire({
+                // icon: "success",
+                imageUrl: 'https://cdn.pixabay.com/photo/2016/10/10/01/49/hook-1727484_150.png',
+                title: "Game Over",
+                text: `Your Score is ${score}`,
+                confirmButtonText: "Play Again",
+                showDenyButton: true,
+            }).then((result) => {
+                console.log(result);
+                if (result.isConfirmed) {
+                    startGame();
+                } else if (result.isDenied) {
+                    console.log("Bye");
+                    $start.classList.remove('hide')
+                }
+            });
+
+            
+        }, 800);
+
+    }
+
+}
+const checkGameState = () => {
+    countStep();
+    calcScore();
+    checkIfGameOver();
+
+};
+
+const calcScore = () => {
+  const rating3Limte = cards.length / 2 + 2,
+    rating2Limte = cards.length,
+    rating1Limte = cards.length * 1.5;
+  const is3Star = steps <= rating3Limte,
+    is2Star = steps >= rating2Limte && steps < rating1Limte,
+    is1Star = steps >= rating2Limte;
+
+  if (is3Star) {
+    score = 3;
+  } else if (is2Star) {
+    score = 2;
+  } else if (is1Star) {
+    score = 1;
+  }
+  $steps.innerHTML = score;
+};
+
+const printCards = (cardsArrray) => {
+  const shuffledCrads = shuffle([...cardsArrray, ...cardsArrray]);
+//   console.log(shuffledCrads);
+$board.innerHTML = '';
+  shuffledCrads.forEach((card) => {
+    const liElement = document.createElement("li");
+    liElement.dataset.answer = card.answer;
+    const imageElement = document.createElement("img");
+    imageElement.src = card.image;
+    imageElement.alt = card.answer;
+    liElement.appendChild(imageElement);
+    $board.appendChild(liElement);
+  });
+};
+
+const startGame = () => {
+
+  printCards(cards);
+  countTime();
+};
+
+const flipCards = (isCorrect) => {
+  setTimeout(() => {
+    const flipCards = Array.from(document.getElementsByClassName("flip"));
+    flipCards.forEach((card) => {
+     
+      if (isCorrect) {
+        card.classList.replace("flip", "open");
+      } else {
+        card.classList.remove("flip");
+      }
+    });
+    $board.classList.remove("compare");
+    checkGameState();
+  }, 800);
+};
+
+$board.addEventListener("click", (e) => {
+  const isCard = e.target.localName === "li";
+  const isOPenCard = e.target.classList.contains("open");
+  const isFlipCard = e.target.classList.contains("flip");
+  // console.log("isOPenCard",isOPenCard);
+
+  if (!isCard || isOPenCard || isFlipCard) {
+    return;
+  }
+  const currentUserSelection = e.target.dataset.answer;
+  e.target.classList.add("flip");
+  selected.push(currentUserSelection);
+  // console.log("selected array length->",selected.length);
+  if (selected.length === 2) {
+    $board.classList.add("compare");
+    const isCorrectAnwser = selected[0] === selected[1];
+    if (isCorrectAnwser) {
+      flipCards(isCorrectAnwser);
+    } else {
+      flipCards(isCorrectAnwser);
+    }
+    selected = [];
+  }
+});
+
+$start.addEventListener('click',()=>{
+  startGame();
+  $start.classList.add('hide')
+});
+// startGame();
+
+Swal.fire({
+    // icon: "question",
+    imageUrl: 'https://cdn.pixabay.com/photo/2016/10/18/18/19/question-mark-1750942_150.png',
+    // title: "Hello",
+    text: "Hi, Welcome Do want to play?",
+    confirmButtonText: "Start Game",
+    showDenyButton: true,
+    // background: '#ffffff url(https://cdn.pixabay.com/photo/2017/12/05/15/45/punctuation-marks-2999583_960_720.jpg) no-repeat center',
+}).then((result) => {
+    console.log(result);
+    if (result.isConfirmed) {
+        startGame();
+        $start.classList.add('hide')
+    } else if (result.isDenied) {
+        console.log("Bye");
+        $start.classList.remove('hide')
+    }
+});
+
+
